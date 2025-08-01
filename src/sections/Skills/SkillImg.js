bf7c18edf5ca8a693d4083878fa79ce3066bfb3d
Kishore Yogeswaran <kishoreyogeswaran@Kishores-MBP.home.local>
2025-08-01 00:11:42 -0400

    Added tooltip

--- src/sections/Skills/SkillImg.js
+++ src/sections/Skills/SkillImg.js
@@ -1,18 +1,27 @@
 import { motion } from "framer-motion";
+import Tippy from "@tippyjs/react";
+import "tippy.js/dist/tippy.css"; // Optional default theme
 
-const skillImg = (data) => {
+const SkillImg = ({ src }) => {
   return (
-    <motion.img
-      className="skills-img"
-      src={require(`../../assets/skills/${data.src}.png`)}
-      alt="skillsImage"
-      whileHover={{ scale: 1.2 }}
-      whileTap={{ scale: 0.8 }}
-      drag
-      dragSnapToOrigin
-      whileDrag={{ scale: 1.5 }}
-    />
+    <Tippy
+      content={src.toUpperCase()}
+      placement="top"
+      arrow={false}
+      delay={[400, 0]}
+    >
+      <motion.img
+        className="skills-img"
+        src={require(`../../assets/skills/${src}.png`)}
+        alt={`${src} logo`}
+        whileHover={{ scale: 1.2 }}
+        whileTap={{ scale: 0.8 }}
+        drag
+        dragSnapToOrigin
+        whileDrag={{ scale: 1.5 }}
+      />
+    </Tippy>
   );
 };
 
-export default skillImg;
+export default SkillImg;
